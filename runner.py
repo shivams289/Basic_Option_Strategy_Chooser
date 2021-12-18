307338072293aa72b04745366ab1f26c9282d532
shivams289 <shivams@shivams-Air.domain.name>
2021-12-19 00:11:36 +0530

    added a strat recaller

--- runner.py
+++ runner.py
@@ -7,9 +7,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from Model.model import Strategy
 
+def wanna_proceed(reply):
+    if reply == True:
+        Call_strategy()
+    else:
+        print('Thank you! See you again.')
+
 def take_inputs():
    
-    val = input('Hey! Welcome to Strategy Choosing Program. Please enter your preferences\n\nAre you bullish?:Enter b\nIf bearish:Enter be\nExpect Underlying to be range bound?:Enter r\nIf Expect Either Side Large Moves?:Enter mn\n\n')
+    val = input('\nHey! Welcome to Strategy Choosing Program. Please enter your preferences\n\nAre you bullish?:Enter b\nIf bearish:Enter be\nExpect Underlying to be range bound?:Enter r\nIf Expect Either Side Large Moves?:Enter mn\n\n')
     
     return val 
 
@@ -43,48 +49,49 @@ def Call_strategy():
     if sentiment == 'mn':
         print('Market Neutral Strategies you can call are:\n1.long_straddle\n2.long_strangle')
 
-    spot = int(input('Enter Current value of spot?\n'))
     inp = NameStrat()
+    spot = int(input('Enter Current value of spot?\n'))
+    
     if inp == 'prbs':
         print("This Strategy requires you to: Buy 2 OTM PE, Sell an ITM PE\n")
         #take input strategy parameter LS, HS, hsp, lsp, 
         LS, HS, hsp, lsp = inp_parameters()
         prbs = Strategy(LS, spot, HS, hsp, lsp)
-        print(prbs.put_ratio_back_spread())
+        prbs.put_ratio_back_spread()
     if inp == 'crbs':
         print("This Strategy requires you to: Buy 2 OTM CE, Sell ITM CE\n")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         crbs = Strategy(LS, spot, HS, hsp, lsp)
-        print(crbs.call_ratio_back_spread())
+        crbs.call_ratio_back_spread()
         
     if inp == 'beps':
         print("This Strategy requires you to: Buy ITM PE, Sell OTM PE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         beps = Strategy(LS, spot, HS, hsp, lsp)
-        print(beps.bear_put_spread())
+        beps.bear_put_spread()
         
     if inp == 'becs':
         print("This Strategy requires you to: Buy OTM CE, Sell ITM CE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         becs = Strategy(LS, spot, HS, hsp, lsp)
-        print(becs.bear_call_spread())
+        becs.bear_call_spread()
         
     if inp == 'bps':
         print("This Strategy requires you to: Buy OTM PE, Sell ITM PE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         bps = Strategy(LS, spot, HS, hsp, lsp)
-        print(bps.bull_put_spread())
+        bps.bull_put_spread()
         
     if inp == 'bcs':
         print("This Strategy requires you to: Buy ITM CE, Sell OTM CE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         bcs = Strategy(LS, spot, HS, hsp, lsp)
-        print(bcs.bull_call_spread())
+        bcs.bull_call_spread()
         
     if inp == 'sstrd':
         print("This Strategy requires you to: Sell ATM CE, Sell ATM PE")
@@ -98,25 +105,32 @@ def Call_strategy():
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         lstrd = Strategy(LS, spot, HS, hsp, lsp)
-        print(lstrd.long_straddle())
+        lstrd.long_straddle()
         
     if inp == 'sstrg':
         print("This Strategy requires you to: Sell OTM CE, Sell OTM PE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         sstrg = Strategy(LS, spot, HS, hsp, lsp)
-        print(sstrg.short_strangle())
+        sstrg.short_strangle()
         
     if inp == 'lstrg':
         print("This Strategy requires you to: Buy OTM CE, Buy OTM PE")
         #take input strategy parameter LS, HS, hsp, lsp,
         LS, HS, hsp, lsp = inp_parameters()
         lstrg = Strategy(LS, spot, HS, hsp, lsp)
-        print(lstrg.long_strangle())
+        lstrg.long_strangle()
 
+if __name__=='__main__':
 
-Call_strategy()
+    Call_strategy()
 
+    recall = input('Wanna test another strategy?\nif yes: Enter y\nif no: Enter n\n')
+    if recall == 'y':
+        recall = True
+    else:
+        recall = False
+    wanna_proceed(recall)
 
 
 
